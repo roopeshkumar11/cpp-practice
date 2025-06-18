60249ac607861cdcd079a3f738fde1d13c29a572
roopeshkumar11 <roopeshkumarbxr2017@gmail.com>
2025-06-18 23:44:27 +0530

    update

--- cpp/Tries/rev/creation.cpp
+++ cpp/Tries/rev/creation.cpp
@@ -4,8 +4,21 @@ using namespace std;
 class TriesNode{
     public:
     char data;
-    TriesNode*children[26]
-}
+    TriesNode*children[26];
+    bool istreminal;
+
+
+
+    TriesNode(char ch){
+    data=ch;
+
+    for(int i=0;i<26;i++){
+        children[i]=NULL;
+    }
+
+    istreminal=false;
+    }
+};
 int main(){
 
 }
