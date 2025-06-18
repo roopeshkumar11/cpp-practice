8c1b6677dae6faeabbf4c4ac05bd4317c81a80eb
roopeshkumar11 <roopeshkumarbxr2017@gmail.com>
2025-06-18 23:56:47 +0530

    Tries

--- cpp/Tries/rev/creation.cpp
+++ cpp/Tries/rev/creation.cpp
@@ -19,6 +19,36 @@ class TriesNode{
     istreminal=false;
     }
 };
+
+class Tries{
+    public:
+
+    TriesNode*root;
+
+    void insertutil(TriesNode*root,string word){
+
+        if(word.length()==0){
+            root->istreminal=true;
+        }
+
+        int index=word[0]-'A';
+        TriesNode*child;
+
+        if(root->children!=NULL){
+            child=root->children[index];
+        }
+
+        else{
+            child=new TriesNode(word[0]);
+            root->children[index]=child;
+        }
+        insertutil(root,word.substr(1));
+    }
+
+    void insert(string word){
+        insertutil(root,word);
+    }
+};
 int main(){
 
 }
