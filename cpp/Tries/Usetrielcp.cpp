07cbb1974212c169e38e3275b012134f865f7a64
roopeshkumar11 <roopeshkumarbxr2017@gmail.com>
2025-06-22 23:55:55 +0530

    tries

---
+++ cpp/Tries/Usetrielcp.cpp
@@ -0,0 +1,99 @@
+class Treinode{
+
+    public:
+    char data;
+    Treinode*children[26];
+    int countnode;
+    bool Terminal;
+
+
+   Treinode(char chh){
+   data=chh;
+   for(int i=0;i<26;i++){
+    children[i]=NULL;
+   }
+   countnode=0;
+
+   Terminal=false;}
+};
+
+
+class Trei{
+    public:
+    Treinode*root;
+
+    Trei(){
+        root=new Treinode('\0');
+    }
+
+
+    void insertutill(Treinode*root,string word){
+        if(word.length()==0){
+         root->Terminal=true;
+         return;
+        }
+
+  int idx=word[0]-'a';
+
+  Treinode*child;
+
+  if(root->children[idx]!=NULL){
+      child=root->children[idx];
+  }
+
+  else{
+        child=new Treinode(word[0]);
+        root->countnode++;
+        root->children[idx]=child;
+  }
+
+    insertutill(child,word.substr(1));
+    }
+     void insertword(string word){
+        insertutill(root,word);
+    }
+
+
+
+void lcp(string wrd,string &ans){
+        for(int i=0;i<wrd.length();i++){
+
+             char ch=wrd[i];
+            if(root->countnode==1){
+                ans+=ch;
+
+                int idx=ch-'a';
+                root=root->children[idx];
+            }
+
+            else{
+                break;
+            }
+
+            if(root->Terminal){
+                break;
+            }
+        }
+    }
+
+};
+
+
+
+string longestCommonPrefix(vector<string> &arr, int n)
+{
+    Trei*t=new Trei();
+    
+    for(int i=0;i<n;i++){
+        t->insertword(arr[i]);
+    }
+
+
+string first=arr[0];
+
+
+    string ans="";
+
+   t->lcp(first,ans);
+    return ans;
+}
